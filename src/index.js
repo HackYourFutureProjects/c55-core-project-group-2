95e82f2bcebf4b4d7c385a27836c307147289b21
Miuroro <monera.mual@gmail.com>
2026-04-13 17:42:04 +0200

    Setup Application Entry Point for Task Tracker Backend in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import { createApp } from '../server.js';
+
+const app = createApp();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
 console.log('Hello World!');
