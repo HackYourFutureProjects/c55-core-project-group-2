50b333a2b64f37f76b5a5946c4a10a4bb5d7d62b
jivvy <jivvyjams@pm.me>
2026-04-19 18:55:00 +0200

    createTaskElement function to add tasks to HTML + escape HTML code

--- public/app.js
+++ public/app.js
@@ -11,10 +11,45 @@ async function loadTasks() {
   renderTasks(tasks);
 }
 
+// Render tasks as HTML elements in web interface
 function renderTasks(tasks) {
   taskList.innerHTML = '';
   for (const task of tasks) {
     taskList.appendChild(createTaskElement(task));
   }
 }
+
+function createTaskElement(task) {
+  const li = document.createElement('li');
+  li.className = 'task';
+  li.dataset.id = task.id;
+
+  li.innerHTML = `
+    <div class="task-left">
+      <input type="checkbox" ${task.status === 'finished' ? 'checked' : ''} />
+      <div class="task-meta">
+        <div class="task-title">${escapeHtml(task.name)}</div>
+        <div class="task-due">${task.deadline ? `Due: ${task.deadline}` : ''}</div>
+      </div>
+    </div>
+    <div class="task-right">
+      <button class="icon delete" title="delete">
+        <i class="nf nf-fa-trash"></i>
+      </button>
+      <span class="priority ${task.importance}"></span>
+    </div>
+  `;
+
+  return li;
+}
+
+// Prevents HTML code from being run by user input
+function escapeHtml(str) {
+  return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+}
+
 loadTasks();
